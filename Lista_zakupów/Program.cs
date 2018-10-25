b5093243ec90f73d5ce2863b83e7f44e623aa329
Jaroslaw Burzawa <jaroslaw.burzawa@balluff.pl>
2018-10-25 22:42:30 +0200

    Dodano sortowanie od najtańszego do najdroższego

--- Lista_zakupów/Program.cs
+++ Lista_zakupów/Program.cs
@@ -14,20 +14,23 @@ namespace Lista_zakupów
             {
             };
             Koszyk KoszJarek = new Koszyk(KoszykJ);
-            Produkt Produkt4 = new Produkt(1, "a", "aa", 10);
-            Produkt Produkt3 = new Produkt(2, "b", "bb", 20);
-            Produkt Produkt2 = new Produkt(3, "c", "cc", 30);
-            Produkt Produkt1 = new Produkt(4, "d", "dd", 40);
+            Produkt Produkt5 = new Produkt(211, "e", "ee", 50);
+            Produkt Produkt4 = new Produkt(111, "a", "aa", 10);
+            Produkt Produkt3 = new Produkt(221, "b", "bb", 20);
+            Produkt Produkt2 = new Produkt(332, "c", "cc", 30);
+            Produkt Produkt1 = new Produkt(401, "d", "dd", 40);
             KoszJarek.DodajDoKoszyka(Produkt1);
             KoszJarek.DodajDoKoszyka(Produkt2);
             KoszJarek.DodajDoKoszyka(Produkt3);
             KoszJarek.DodajDoKoszyka(Produkt4);
+            KoszJarek.DodajDoKoszyka(Produkt5);
             KoszJarek.WyswietlKoszyk();
-            /*KoszJarek.UsunZKoszyka(3);
-            KoszJarek.WyswietlKoszyk();*/
             Produkt1.PodajCene();
             KoszJarek.PodajCene();
             KoszJarek.SortujPoCenach();
+            KoszJarek.WyswietlKoszyk();
+            Console.WriteLine("");
+            KoszJarek.UsunZKoszyka(221);
             KoszJarek.WyswietlKoszyk();
             Console.ReadLine();
         }
